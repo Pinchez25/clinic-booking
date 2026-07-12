bce79a89111cee18d2ccebb52c8270b850592190
Pinchez25 <thuamwangi001@gmail.com>
2026-07-13 01:11:35 +0300

    feat(admin): register User model with custom UserAdmin configuration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ["email", "first_name", "last_name", "role", "is_active", "date_joined"]
+    list_filter = ["role", "is_active", "is_staff"]
+    search_fields = ["email", "first_name", "last_name"]
+    ordering = ["-date_joined"]
+    list_select_related = []
+
+    fieldsets = (*BaseUserAdmin.fieldsets, ("Role", {"fields": ("role",)}))
+    add_fieldsets = (*BaseUserAdmin.add_fieldsets, ("Role", {"fields": ("role",)}))
