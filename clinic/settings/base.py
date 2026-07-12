2d0141de114790d848f3eadc51cfae1378b29fd1
Pinchez25 <thuamwangi001@gmail.com>
2026-07-12 15:36:43 +0300

    feat(database): switch to PostgreSQL and configure database settings using environment variables

--- clinic/settings/base.py
+++ clinic/settings/base.py
@@ -64,9 +64,15 @@ WSGI_APPLICATION = 'clinic.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "CONN_MAX_AGE": 600,
+        "CONN_HEALTH_CHECKS": True,
     }
 }
 
