eb5ffd7e23d49308a47547a8340fe411c799ecbd
Pinchez25 <thuamwangi001@gmail.com>
2026-07-13 01:23:30 +0300

    feat(settings): add REST framework and related packages to installed apps

--- clinic/settings/base.py
+++ clinic/settings/base.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts.apps.AccountsConfig",
+    "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
+    "django_filters",
 ]
 
 AUTH_USER_MODEL = "accounts.User"
